33fdefc9130aaf0aa9047fa0332a6d603390bb07
vittorio scocca <vittorio.scocca@pynlab.com>
2026-02-04 13:54:02 +0100

    FirstPush

---
+++ KidBox/KidBox/KidBoxApp.swift
@@ -0,0 +1,32 @@
+//
+//  KidBoxApp.swift
+//  KidBox
+//
+//  Created by vscocca on 04/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KidBoxApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
