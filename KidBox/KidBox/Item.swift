c09d6344773dfd09f6e59d0d6bbb13fddb06cf66
vittorio scocca <vittorio.scocca@pynlab.com>
2026-02-04 13:54:02 +0100

    FirstPush

---
+++ KidBox/KidBox/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KidBox
+//
+//  Created by vscocca on 04/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
